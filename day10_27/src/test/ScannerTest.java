41d98321f04baedbd83aed74ba936a0f9594e758
LiuZiQiao <1844861420@qq.com>
2018-10-27 21:43:35 +0800

    new file:   day10_27/.classpath 	new file:   day10_27/.gitignore 	new file:   day10_27/.project new file:   day10_27/.settings/org.eclipse.jdt.core.prefs 	new file:   day10_27/src/test/ScannerTest.java

---
+++ day10_27/src/test/ScannerTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+/**
+* @author 作者  小小刘
+* @version 创建时间：2018年10月27日 上午10:45:33
+* 类说明
+*/
+public class ScannerTest {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
